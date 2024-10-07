0447493a96a304f09e0ac6db75ab519f72492ce2
wflore19 <wflore19@montgomerycollege.edu>
2024-10-07 18:13:48 -0400

    fix: footer

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -126,7 +126,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Oyster, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
